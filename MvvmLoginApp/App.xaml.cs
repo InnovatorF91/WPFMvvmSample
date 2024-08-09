d9e926afe8f1c43de8c18801c2be66bebf11dba7
InnovatorF91 <57931103+InnovatorF91@users.noreply.github.com>
2024-08-10 01:47:41 +0900

    Add files via upload

--- MvvmLoginApp/App.xaml.cs
+++ MvvmLoginApp/App.xaml.cs
@@ -4,11 +4,5 @@ namespace MvvmLoginApp
 {
 	public partial class App : Application
 	{
-		protected override void OnStartup(StartupEventArgs e)
-		{
-			base.OnStartup(e);
-			MainWindow = new MainWindow();
-			MainWindow.Show();
-		}
 	}
 }
